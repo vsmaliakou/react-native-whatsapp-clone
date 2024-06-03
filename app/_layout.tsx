9b8c1d1af5d1f5195edc1e01250412f6943e25e5
vsmaliakou <vsmaliakou@yandex.by>
2024-06-03 22:46:58 +0300

    FEAT: Create Welcome and OTP screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,26 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/components/useColorScheme';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -43,7 +45,14 @@ export default function RootLayout() {
 function RootLayoutNav() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="otp"
+        options={{
+          headerTitle: "Enter Your Phone Number",
+          headerBackVisible: false,
+        }}
+      />
     </Stack>
   );
 }
---
+++ app/otp.tsx
@@ -0,0 +1,185 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  Linking,
+  TouchableOpacity,
+} from "react-native";
+import Colors from "@/constants/Colors";
+import { useState } from "react";
+import { useRouter } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import MaskInput from "react-native-mask-input";
+
+const BEL_PHONE = [
+  "+",
+  /\d/,
+  /\d/,
+  /\d/,
+  " ",
+  /\d/,
+  /\d/,
+  " ",
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+];
+
+const Page = () => {
+  const [loading, setLoading] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const router = useRouter();
+  const keyboardVerticalOffset = Platform.OS === "ios" ? 90 : 0;
+  const { bottom } = useSafeAreaInsets();
+
+  const openLink = () => {
+    Linking.openURL("https://github.com/vsmaliakou");
+  };
+
+  const sendOTP = async () => {};
+
+  const trySignIn = async () => {};
+
+  return (
+    <KeyboardAvoidingView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          WhatsApp will need to verify your account. Carrier charges may apply.
+        </Text>
+
+        <View style={styles.list}>
+          <View style={styles.listItem}>
+            <Text style={styles.listItemText}>Belarus</Text>
+
+            <Ionicons name="chevron-forward" size={20} color={Colors.gray} />
+          </View>
+
+          <View style={styles.separator} />
+
+          <MaskInput
+            style={styles.input}
+            value={phoneNumber}
+            keyboardType="numeric"
+            autoFocus
+            placeholder="+375 your phone number"
+            onChangeText={(masked, unmasked) => {
+              setPhoneNumber(masked);
+            }}
+            mask={BEL_PHONE}
+          />
+        </View>
+
+        <Text style={styles.legal}>
+          You must be{" "}
+          <Text style={styles.link} onPress={openLink}>
+            at least 16 years old
+          </Text>{" "}
+          to register. Learn how WhatsApp works with the{" "}
+          <Text style={styles.link} onPress={openLink}>
+            Meta Companies
+          </Text>
+          .
+        </Text>
+
+        <View style={{ flex: 1 }} />
+
+        <TouchableOpacity
+          style={[
+            styles.button,
+            phoneNumber !== "" ? styles.enabled : null,
+            { marginBottom: bottom },
+          ]}
+          onPress={sendOTP}
+          disabled={phoneNumber === ""}
+        >
+          <Text
+            style={[
+              styles.buttonText,
+              phoneNumber !== "" ? styles.enabled : null,
+            ]}
+          >
+            Next
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: Colors.background,
+    gap: 20,
+  },
+  description: {
+    fontSize: 14,
+    color: Colors.gray,
+  },
+  list: {
+    backgroundColor: "#ffffff",
+    width: "100%",
+    borderRadius: 10,
+    padding: 10,
+  },
+  listItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 6,
+    marginBottom: 10,
+  },
+  listItemText: {
+    fontSize: 18,
+    color: Colors.primary,
+  },
+  separator: {
+    width: "100%",
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: Colors.gray,
+    opacity: 0.3,
+  },
+  legal: {
+    fontSize: 12,
+    textAlign: "center",
+    color: "#000000",
+  },
+  link: {
+    color: Colors.primary,
+  },
+  button: {
+    width: "100%",
+    alignItems: "center",
+    backgroundColor: Colors.lightGray,
+    padding: 10,
+    borderRadius: 10,
+  },
+  enabled: {
+    backgroundColor: Colors.primary,
+    color: "#ffffff",
+  },
+  buttonText: {
+    color: Colors.gray,
+    fontSize: 22,
+    fontWeight: 500,
+  },
+  input: {
+    backgroundColor: "#ffffff",
+    width: "100%",
+    fontSize: 16,
+    padding: 6,
+    marginTop: 10,
+  },
+});
+
+export default Page;
